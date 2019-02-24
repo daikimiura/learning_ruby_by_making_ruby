abaa3751bf258e2a5aacb928b852797dedc1f544
daikimiura <daikiii5555@gmail.com>
2019-02-24 21:56:43 +0900

    4.5

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,18 @@
 require 'minruby'
 
-tree = minruby_parse("1 + 2 * 4")
-p(tree)
+def sum(tree)
+  if tree[0] == 'lit'
+    tree[1]
+  else
+    left = sum(tree[1])
+    right = sum(tree[2])
+    left + right
+  end
+end
+
+# str = gets
+# tree = minruby_parse(str)
+tree = minruby_parse("(1 + 2) + (3 + 4)")
+# answer =
+answer = sum(tree)
+p(answer)
